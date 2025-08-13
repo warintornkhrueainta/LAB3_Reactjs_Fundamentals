461905915d32d7585e584a56e7805beace288640
warintorn <warintorn0987716154@gmail.com>
2025-08-13 10:51:37 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-workshop/', // ต้องมี slash นำหน้าและปิดท้าย
   plugins: [react()],
-  base: 'LAB3_Reactjs_Fundamentals', // สำหรับ GitHub Pages ใช้ '/<repository-name>/'
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false, // ปิดสำหรับ production
-  }
 })
