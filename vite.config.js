ed8c7fb5ca1207b6a5ad9525b22b6598052c8364
warintorn <warintorn0987716154@gmail.com>
2025-08-13 10:32:21 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // สำหรับ GitHub Pages ใช้ '/<repository-name>/'
+  base: 'LAB3_Reactjs_Fundamentals', // สำหรับ GitHub Pages ใช้ '/<repository-name>/'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
