edc2488a42f8b66e10438578b54649cbb5234818
warintorn <warintorn0987716154@gmail.com>
2025-08-13 10:26:40 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // สำหรับ GitHub Pages ใช้ '/<repository-name>/'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false, // ปิดสำหรับ production
+  }
 })
